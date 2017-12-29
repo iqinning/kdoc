4c1402597e48c3e18af6382be80276051aa35596
wangjianliang <wangjianliang@kingsoft.com>
2017-12-29 16:13:55 +0800

    change

--- bin/kdoc.js
+++ bin/kdoc.js
@@ -13,7 +13,7 @@ const list = function(val) {
 program
     .version("0.1.0")
     .option(
-        "-p, --plugins <plugins>",
+        "-p, --plugins <plugins> <options>",
         "add plugins,可以被require引入的模块名,或js文件路径,逗号分隔",
         list
     )
